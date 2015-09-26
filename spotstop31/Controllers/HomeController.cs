034f3fe56b8185f1ba10db3b49cebf9b3ad1ae17
cowtongue <revisedboomerguy@gmail.com>
2015-09-26 18:06:26 -0500

    Add Styles

--- spotstop31/Controllers/HomeController.cs
+++ spotstop31/Controllers/HomeController.cs
@@ -27,6 +27,30 @@ namespace spotstop31.Controllers
             return View();
         }
 
+        public ActionResult SomeActionMethod()
+        {
+            spotstop31.Models.SearchQuery sq = new Models.SearchQuery();
+
+            sq.latitude = 50.000;
+            sq.longitude = 60.000;
+            sq.startTime = new DateTime(2015, 9, 25);
+            sq.endTime = new DateTime(2015, 9, 26);
+            sq.radius = 30.00;
+
+            Random random = new Random();
+
+            for (int i=0; i<100; i++)
+            {
+                
+                double randomNumberLat = 0.000000;
+                double randomNumberLong = 0.000000;
+                randomNumberLat += random.Next(-100, 100);
+                randomNumberLong += random.Next(-100, 100);
+
+            }
+            return Json(sq, JsonRequestBehavior.AllowGet);
+        }
+
         public ContentResult Info()
         {
             return Content("Data");
---
+++ spotstop31/Models/SearchReturn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace spotstop31.Models
+{
+    public class SearchReturn
+    {
+        public double latitude = 30.2848691d;
+        public double longitude = -97.7364655d;
+        public double radius;
+        public int startTime;
+        public int endTime;
+        public int amountOfSpots;
+        public string specialInstructions;
+    }
+
+    public class SearchQuery
+    {
+        public double latitude = 30.2848691d;
+        public double longitude = -97.7364655d;
+        public double radius;
+        public DateTime startTime;
+        public DateTime endTime;
+        public int amountOfSpots;
+    }
+
+
+}
