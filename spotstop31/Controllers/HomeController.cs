4123654077359d22e7d6798726df585c8adb88e7
cowtongue <revisedboomerguy@gmail.com>
2015-09-26 18:36:54 -0500

    Added location and radius

--- spotstop31/Controllers/HomeController.cs
+++ spotstop31/Controllers/HomeController.cs
@@ -27,28 +27,38 @@ namespace spotstop31.Controllers
             return View();
         }
 
-        public ActionResult SomeActionMethod()
+        public ActionResult NewSearch (double location, int radius)
         {
-            spotstop31.Models.SearchQuery sq = new Models.SearchQuery();
-
-            sq.latitude = 50.000;
-            sq.longitude = 60.000;
-            sq.startTime = new DateTime(2015, 9, 25);
-            sq.endTime = new DateTime(2015, 9, 26);
-            sq.radius = 30.00;
-
             Random random = new Random();
+            double distancemagnitude = 0;
+            List<spotstop31.Models.SearchQuery> s = new List<spotstop31.Models.SearchQuery>();
 
-            for (int i=0; i<100; i++)
+            for (int i = 0; i < 100; i++)
             {
-                
+                spotstop31.Models.SearchQuery sq = new Models.SearchQuery();
                 double randomNumberLat = 0.000000;
                 double randomNumberLong = 0.000000;
-                randomNumberLat += random.Next(-100, 100);
-                randomNumberLong += random.Next(-100, 100);
+                randomNumberLat = random.NextDouble() * (100);
+                randomNumberLong = random.NextDouble() * (100);
+                distancemagnitude = randomNumberLat * randomNumberLat + randomNumberLong * randomNumberLong;
+                distancemagnitude = Math.Sqrt(distancemagnitude);
 
+                if (distancemagnitude > radius)
+                {
+                    continue;
+                }
+                else
+                {
+                    sq.latitude = randomNumberLat;
+                    sq.longitude = randomNumberLong;
+                    sq.startTime = new DateTime(2015, 9, 25);
+                    sq.endTime = new DateTime(2015, 9, 26);
+                    sq.radius = 30.00;
+                    s.Add(sq);
+                }
             }
-            return Json(sq, JsonRequestBehavior.AllowGet);
+
+            return Json(s, JsonRequestBehavior.AllowGet);
         }
 
         public ContentResult Info()
