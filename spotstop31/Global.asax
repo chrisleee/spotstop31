a7005760ed8b2fcd0f52ef55a2c5204aece6c4e9
cowtongue <revisedboomerguy@gmail.com>
2015-09-26 16:54:41 -0500

    Initial commit

---
+++ spotstop31/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="spotstop31.MvcApplication" Language="C#" %>
---
+++ spotstop31/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(spotstop31.Startup))]
+namespace spotstop31
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
