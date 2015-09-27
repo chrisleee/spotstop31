8067136fed2fe71945cf2e4eecd58f4ed03a13cd
Jackson Haenchen <jacksonhaenchen@gmail.com>
2015-09-27 05:14:09 -0700

    testing coloring

--- spotstop31/Scripts/scripts.js
+++ spotstop31/Scripts/scripts.js
@@ -168,7 +168,7 @@ function addElements(data) {
         var spots = data[i].amountOfSpots;
         console.log(data[i]);
         var latLng = new google.maps.LatLng(lat, lng);
-        var div = $("<div>").addClass("panel panel-default").attr("id", i);
+        var div = $("<div>").addClass("panel panel-default our-panel").attr("id", i);
         var thereString;
         var spotsString;
         if (spots === 1) {
