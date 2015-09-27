8695662388d15d5bed3fbbd2a81990f4bd5b1b8a
Austinbm <austin.bm@Hotmail.com>
2015-09-26 20:29:32 -0500

    Revert marker

--- spotstop31/Scripts/scripts.js
+++ spotstop31/Scripts/scripts.js
@@ -6,18 +6,7 @@ $("#submit").click(function(){
     console.log("clicked");
     var latLong = $("#latLong").val();
     var arraylatLong = latLong.split(" ");
-<<<<<<< HEAD
-    changeMap(arraylatLong[0],arraylatLong[1]);
-    /*placeMarkers(makeArray()); */
-=======
     changeMap(arraylatLong[0],arraylatLong[1])
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 16928e7... place markers
-=======
->>>>>>> parent of 16928e7... place markers
-=======
->>>>>>> parent of 16928e7... place markers
   });
 
 function initialize() {
@@ -64,32 +53,3 @@ function changeMap(lat,lng) {
 };
 /* end google maps -----------------------------------------------------*/
 });
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-/*
-function makeArray() {
-  var points = [[30.2881, -97.7473]];
-  return points;
-}
-
-function placeMarkers(points) {
-  for (var i = 0;i<points.length;i++){
-    var lat = points[i][0];
-    var lng = points[i][1];
-    var latlng = new google.maps.LatLng(lat, lng);
-    var marker2 = new google.maps.Marker({
-    position: latlng,
-    url: '/',
-    animation: google.maps.Animation.DROP;
-    
-  });
-    marker2.setMap(map);
-  }
-}
-=======
->>>>>>> parent of 16928e7... place markers
-=======
->>>>>>> parent of 16928e7... place markers
-=======
->>>>>>> parent of 16928e7... place markers
